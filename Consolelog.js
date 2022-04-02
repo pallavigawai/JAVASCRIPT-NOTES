511ee3728f85e5fdbc469f5566a768d469159676
pallavi <99071482+pallavigawai@users.noreply.github.com>
2022-04-02 19:20:58 +0530

    Update Consolelog.js

--- Consolelog.js
+++ Consolelog.js
@@ -1,16 +1,16 @@
-Programming always includes the search for errors. One way to find errors is logging. During program execution, selective outputs are made. To do this in JavaScript, use the console.log function:
+//Programming always includes the search for errors. One way to find errors is logging. During program execution, selective outputs are made. To do this in JavaScript, use the console.log function:
 
-console.log('Hello console!');
-Here, console.log outputs 'Hello console!'
+//console.log('Hello console!');
+//Here, console.log outputs 'Hello console!'
 
-e.g  
-1]Write a function log, that takes a parameter and logs this parameter.
+//e.g  
+//1]Write a function log, that takes a parameter and logs this parameter.
   Example: log('Ken Thompson') should log 'Ken Thompson'.
    function log(name) {
      console.log(name);
     }
     
- 2] Write a function shout that takes a string and returns this string duplicated. In addition, the return should be logged.
+// 2] Write a function shout that takes a string and returns this string duplicated. In addition, the return should be logged.
  function shout(word){
  let result word + word;
  console.log(result);
