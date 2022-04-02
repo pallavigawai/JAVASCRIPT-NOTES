78d4d15d683aabbed5af3537bec7a958697b7517
pallavi <99071482+pallavigawai@users.noreply.github.com>
2022-04-02 19:23:13 +0530

    Update Null  & Undefined.js

--- Null  & Undefined.js
+++ Null  & Undefined.js
@@ -1,8 +1,8 @@
-Null:- represents the intentional absence of any object value. It is one of JavaScript's primitive values and is treated as falsy for boolean operations.
+//Null:- represents the intentional absence of any object value. It is one of JavaScript's primitive values and is treated as falsy for boolean operations.
  
 /******* Difference between null and undefined *******/
-When checking for null or undefined, beware of the differences between equality (==) and identity (===) operators, as the former performs type-conversion.
-
+//When checking for null or undefined, beware of the differences between equality (==) and identity (===) operators, as the former performs type-conversion.
+/*
 typeof null          // "object" (not "null" for legacy reasons)
 typeof undefined     // "undefined"
 null === undefined   // false
@@ -12,9 +12,9 @@ null  == null        // true
 !null                // true
 isNaN(1 + null)      // false
 isNaN(1 + undefined) // true
-
-e.g:-
-1]Write a function cutComment that takes one line of JavaScript and returns a possible line comment trimmed. If the line contains no line comment, null should be returned. For simplicity, we assume that the code does not contain the comment characters within a string.
+*/
+//e.g:-
+//1]Write a function cutComment that takes one line of JavaScript and returns a possible line comment trimmed. If the line contains no line comment, null should be returned. For simplicity, we assume that the code does not contain the comment characters within a string.
 const cutComment = string => {
 let onlyComment = string.indexOf('//');
 if (string.indexOf('/') < 1) {
