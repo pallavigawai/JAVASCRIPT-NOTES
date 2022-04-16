6ce177467453124fd20d4b72e16028e121a224ed
pallavi <99071482+pallavigawai@users.noreply.github.com>
2022-04-16 14:18:43 +0530

    Update arrayAt&Length.js

--- arrayAt&Length.js
+++ arrayAt&Length.js
@@ -19,3 +19,33 @@ e.g. array[array.length-1] for the last item, you can call array.at(-1): this gi
 const item1 = returnLast(cart);
 console.log(item1); // Logs: 'pear'
 
+/*
+             ARRAY LENGTH 
+  Returns the number of elements in that array.
+  
+ */
+//When you extend an array by changing its length property, the number of actual elements increases
+const arr = [1, 2];
+console.log(arr);
+// [ 1, 2 ]
+arr.length = 5; // set array length to 5 while currently 2.
+console.log(arr);
+// [ 1, 2, <3 empty items> ]
+arr.forEach(element => console.log(element));
+// 1
+// 2
+
+// Iterating over an array
+ const numbers = [1, 2, 3, 4, 5];
+const length = numbers.length;
+for (var i = 0; i < length; i++) {
+  numbers[i] *= 2;
+}
+// numbers is now [2, 4, 6, 8, 10]
+
+//Create empty array of fixed length
+const numbers = [];
+numbers.length = 3;
+console.log(numbers); // [undefined, undefined, undefined]
+
+//Note:-  In array for loop, always starts from 0 i.e (i = 0)
