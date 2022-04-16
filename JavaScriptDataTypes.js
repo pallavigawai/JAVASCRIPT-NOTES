ae07867cfa4a456165c7af16efed4d9607d469c6
pallavi <99071482+pallavigawai@users.noreply.github.com>
2022-04-17 00:13:25 +0530

    Create JavaScriptDataTypes.js

---
+++ JavaScriptDataTypes.js
@@ -0,0 +1,16 @@
+/************************ JAVASCRIPT DATA TYPES ************************ /
+
+There are eight basic data types in JavaScript. They are:
+
+Data Types	Description	Example
+1.String	represents textual data	'hello', "hello world!" etc
+2.Number	an integer or a floating-point number	3, 3.234, 3e-2 etc.
+3.BigInt	an integer with arbitrary precision	900719925124740999n , 1n etc.
+4.Boolean	Any of two values: true or false	true and false
+5.undefined	a data type whose variable is not initialized	let a;
+6.null	denotes a null value	let a = null;
+7.Symbol	data type whose instances are unique and immutable	let value = Symbol('hello');
+8.Object	key-value pairs of collection of data	let student = { };
+  Here, all data types except Object are primitive data types, whereas Object is non-primitive.
+
+Note: The Object data type (non-primitive type) can store collections of data, whereas primitive data type can only store a single data.
