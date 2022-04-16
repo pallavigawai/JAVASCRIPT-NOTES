bb5ceeaa9d85948cbc62b85ebb262f933771e2cb
pallavi <99071482+pallavigawai@users.noreply.github.com>
2022-04-16 13:59:58 +0530

    Update and rename array_at & length.js to arrayAt&Length.js

--- arrayAt&Length.js
+++ arrayAt&Length.js
@@ -3,17 +3,17 @@
 //The at() method takes an integer value and returns the item at that index, allowing for positive and negative integers.
 //Negative integers count back from the last item in the array.
 
-//For example
-//array[0] :- return the first item. 
-//instead of using array.length for latter items; 
-//e.g. array[array.length-1] for the last item, you can call array.at(-1)
-
-Return the last value of an array:-
+For example
+array[0] :- return the first item. 
+instead of using array.length for latter items; 
+e.g. array[array.length-1] for the last item, you can call array.at(-1): this gives last index of element.
+*/
+// Return the last value of an array:-
 1]const cart = ['apple', 'banana', 'pear'];
  // A function which returns the last item of a given array
  function returnLast(arr) {
    return arr.at(-1);
-   //return ary[ary.length-1];
+   //return ary[ary.length-1]; (OR)
  }
 // Get the last item of our array 'cart'
 const item1 = returnLast(cart);
