d271d3cad9be97762381ef91611cbb1fec32f1fd
pallavi <99071482+pallavigawai@users.noreply.github.com>
2022-04-25 14:43:52 +0530

    Create aboutJavaScript.js

---
+++ aboutJavaScript.js
@@ -0,0 +1,32 @@
+/************* WHAT YOU SHOULD KNOW ABOUT JAVASCRIPT *************** /
+
+ WHAT IS JAVASCRIPT?
+* High level, interpreted Programmming language
+* Conforms to the ECMAScript specification
+* Runs on the client/browser as well as on the server (Node.js)
+
+ WHY LEARN JAVASCRIPT?
+* It is programming language of the browser
+* Build very interactive user interfaces with frameworks like React
+* used in building very fast server side and full stack application
+* Used in mobile application(React Native, NativeScript,Ionic)
+* used in Desktop application (Electron JS)
+# JavaScript was previously used mainly for making webpages interactive such as form validation, animation, etc. Nowadays, JavaScript is also used in many other areas such as server-side development, mobile app development and so on.
+  Because of its wide range of applications, you can run JavaScript in several ways:
+* Using console tab of web browsers
+* Using Node.js
+* By creating web pages
+
+WHAT YOU WILL LEARN IN THIS COURSE
+* Variables & Data Types
+* Arrays
+* Object Literals
+* Method for strings, arrays, objects, etc
+* Loops - for, While, for..of, forEach, map
+* condtionals (if, ternary & switch)
+* Function ( normal, arrow)
+* DOM Selection
+* DOM Manipulation
+* Events
+* Basic form Validation.
+
