aff4448dc0a8aeb52df3e56fe489ca132c06e802
pallavi <99071482+pallavigawai@users.noreply.github.com>
2022-07-09 23:59:23 +0530

    Create HigherOrderFunction.js

---
+++ HigherOrderFunction.js
@@ -0,0 +1,8 @@
+/********************************* HIGHER- ORDER FUNCTION **************************** /
+
+* A Higher-Order function is a function that receives a function as an argument otherwise returns function as output.
+* The Javascript Array object which has a lot of higher-order functions which some of them like sort, map, reduce, and filter.
+* Higher-order functions help you to manipulate an array as a whole.
+ # Map
+A map allows you to pass a function that applies to every element of an Array that is why we call it a higher-order function.
+The map function does not alter the original array but uses the original array to create a new array that is acted upon by the functions passed on the map argument function.
