2120a30ea2fe0c0f5f9fb07c174076c401d3aac1
pallavi <99071482+pallavigawai@users.noreply.github.com>
2022-07-11 00:39:00 +0530

    Update HigherOrderFunction&Arrays.js

--- HigherOrderFunction&Arrays.js
+++ HigherOrderFunction&Arrays.js
@@ -7,6 +7,12 @@ USE OF HIGHER ORDER FUNCTION:
 * These higher-order functions decrease the length of the code,
 * Increase the readability and simplify the accessibility.
 * Some of the higher-order functions are maps, filters and reduce. 
+* In JavaScript, the functions can use primitive types (like numbers, strings), objects (like arrays, plain objects, regular expressions, etc) as arguments, and return a primitive type or object too.
+* The functions that use other functions as arguments or return functions are named higher-order functions.
+* The higher-order functions help reduce the code duplication and favor the single-responsibility principle.
+* Higher-order functions in JavaScript are a special category of functions that either accept functions as an argument or return functions.
+* On the other side, if the function uses only primitives or objects as arguments or return values, these functions are first-order.
+* Higher-order functions provide the reusability benefit: the main behavior is provided by the higher-order function itself, and by accepting a function as an argument you extend that behavior at your will.
 
 In the following example, a new array is taken, even though the provided array is enough to complete the task. 
 A for-loop, which is unnecessary in case of higher-order functions, is taken to loop through the array. 
